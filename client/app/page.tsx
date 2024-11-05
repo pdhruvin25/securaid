3b9806682e5feb89f9d77cdb25fe6ce7fcddd246
Dhruvin Patel <pateldhruvin2503@gmail.com>
2024-11-04 20:34:08 -0500

    Initial setup

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,5 +1,16 @@
+import React, { useEffect, useState } from 'react'
 
 function page() {
+
+  const [message, setMessage] = useState("Loading")
+
+  useEffect(() => {
+    fetch("http://localhost:3000/api/home").then(response => response.json()).then(data => {
+      console.log(data);
+      setMessage(data.message);
+    })
+  }, [])
+
   return (
     <div>Hello World</div>
   )
--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
+const cors =require('cors');
 const PORT = 4000;
-
+app.use(cors());
 app.get("/api/home", (req, res) => {
 res.json({message: "hello world"});
 })
